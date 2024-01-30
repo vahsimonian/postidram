c814b3e62ae30f5d624bf61a86dad7cfaf956e95
vahsimonian <simonianvah@gmail.com>
2024-01-30 22:56:21 +0700

    commit

--- server/index.js
+++ server/index.js
@@ -27,9 +27,13 @@ app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
 // FILE STORAGE
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
+    cb(null, 'public/assets');
+  },
+  filename: function (req, file, cb) {
     cb(null, file.originalname);
   },
 });
+
 const upload = multer({ storage });
 
 // MONGOOSE SETUP
