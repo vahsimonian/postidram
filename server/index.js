d84e0e6c73b0bea8a1a5948ad49895e079bd6141
vahsimonian <simonianvah@gmail.com>
2024-01-30 22:39:10 +0700

    commit

--- server/index.js
+++ server/index.js
@@ -10,7 +10,6 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 // CONFIGURATIONS
-// CONFIGURATIONS
 
 const __fileName = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__fileName);
@@ -24,3 +23,24 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+
+// FILE STORAGE
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
+
+// MONGOOSE SETUP
+
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
