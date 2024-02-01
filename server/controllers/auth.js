937925824255255449f1117208260092ebce6332
vahsimonian <simonianvah@gmail.com>
2024-02-01 19:09:59 +0700

    commit

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -44,12 +44,15 @@ export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email: email });
+    // const user = await User.findOne({ email });
     if (!user) return res.status(400).json({ msg: 'User does not exist.' });
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ msg: 'Invalid credentials' });
 
-    const token = jwt.token({ id: user.id }, process.env.JWT_SECRET);
+    const token = jwt.token({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+    res.status(200).json({ token, user });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
