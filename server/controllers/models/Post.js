933bc8fa7c3b4a3328c0b61f3183ec7d051559c5
vahsimonian <simonianvah@gmail.com>
2024-02-06 11:05:52 +0700

    commit

--- server/controllers/models/Post.js
+++ server/controllers/models/Post.js
@@ -1,24 +1,35 @@
 import mongoose from 'mongoose';
 
-const postSchema = mongoose.Schema({
-  userId: {
-    type: String,
-    required: true,
+const postSchema = mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    location: String,
+    descripiton: String,
+    picturePath: String,
+    userPicturePath: String,
+    likes: {
+      type: Map,
+      of: Boolean,
+    },
+    comments: {
+      types: Array,
+      default: [],
+    },
   },
-  firstName: {
-    type: String,
-    required: true,
-  },
-  lastName: {
-    type: String,
-    required: true,
-  },
-  location: String,
-  descripiton: String,
-  picturePath: String,
-  userPicturePath: String,
-  likes: {
-    type: Map,
-    of: Boolean,
-  },
-});
+  { timestamp: true }
+);
+
+const Post = mongoose.model('Post', postSchema);
+
+export default Post;
