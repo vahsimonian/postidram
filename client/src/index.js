df358388033295c92a8e2c820f479ddc4e34a552
vahsimonian <simonianvah@gmail.com>
2024-02-22 19:30:36 +0700

    commit

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import authReducer from './state';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/es/integration/react';
+import { buildGetDefaultMiddleware } from '@reduxjs/toolkit/dist/getDefaultMiddleware';
+
+const persistConfig = { key: 'root', storage, version: 1 };
+const persistedReducer = persistedReducer(persistConfig, authReducer);
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: buildGetDefaultMiddleware({
+    serializableCheck: {
+      ignoredAcions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistStore(store)}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
