05e84f9e997b9f0ed09e3ed6c5d63b6ade51a848
vahsimonian <simonianvah@gmail.com>
2024-02-22 20:22:10 +0700

    commit

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import authReducer from './state';
 import { configureStore } from '@reduxjs/toolkit';
-import { Provider } from 'react';
+import { Provider } from 'react-redux';
 import {
   persistStore,
   persistReducer,
@@ -17,17 +17,17 @@ import {
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { PersistGate } from 'redux-persist/es/integration/react';
-import { buildGetDefaultMiddleware } from '@reduxjs/toolkit/dist/getDefaultMiddleware';
 
 const persistConfig = { key: 'root', storage, version: 1 };
-const persistedReducer = persistedReducer(persistConfig, authReducer);
+const persistedReducer = persistReducer(persistConfig, authReducer);
 const store = configureStore({
   reducer: persistedReducer,
-  middleware: buildGetDefaultMiddleware({
-    serializableCheck: {
-      ignoredAcions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-    },
-  }),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
